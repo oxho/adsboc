b72ace70d72a19bb7d836789a1f98190eaf3eacc
oxho <oxho@oxhodeMacBook-Pro.local>
2022-01-08 11:31:57 +0800

    新增详情VC

---
+++ AdsLoadImageboc/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  AdsLoadImageboc
+//
+//  Created by oxho on 2022/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
